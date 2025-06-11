e87c673844b0ea90769da4c4128f93190813c822
charleschiu075 <ccharles0705@gmail.com>
2025-06-11 19:58:42 +0800

    Fix ip, port, sytaxError

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 dotenv.config();
@@ -10,5 +9,6 @@ const app = express();
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/stream', require('./routes/stream'));
+app.use(express.static(path.join(__dirname, '../public')));
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
